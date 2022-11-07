5414c28cadb5dc86d4fd9a6bcbad3c684f9e6c4c
piyushswain <piyush.swain3@gmail.com>
2022-11-07 14:34:05 +0530

    Fix issue in variants_inr.rb seed file
    There was an issue where price was not available and keyword `frozen_string_literal` was not present on the file.
    This has been fixed now.

--- db/samples/variants_inr.rb
+++ db/samples/variants_inr.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Spree::Variant.all.each do |variant|
   variant.cost_currency = 'INR'
-  variant.price = variant.price * 10
+  variant.price = variant.cost_price * 10
   variant.save!
 end

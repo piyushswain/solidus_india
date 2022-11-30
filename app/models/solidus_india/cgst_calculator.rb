b4558e94e808f75cd690565f0341e0c139738490
piyushswain <piyush.swain3@gmail.com>
2022-11-30 13:48:10 +0530

    Add Seeds for Tax Rates and Zones
    This commit adds seeds for Indian Tax rates and zones. It creates tax categories with these tax rates.
    
    The created tax_categories are applied to products so that taxes can be processed during the order calculation by the tax calculators added in this commit.

---
+++ app/models/solidus_india/cgst_calculator.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_dependency 'spree/calculator'
+
+module SolidusIndia
+  class CgstCalculator < Spree::Calculator
+    def compute_line_item(line_item)
+      calculate_rates(line_item)
+    end
+
+    def compute_shipment(shipment)
+      calculate_rates(shipment)
+    end
+
+    def compute_shipping_rate(shipping)
+      calculate_rates(shipping)
+    end
+
+    private
+
+    def calculate_rates(item)
+      tax_rate = Spree::TaxRate.find_by(name: 'CGST')
+      item.amount * tax_rate.amount
+    end
+  end
+end
